b33d8af78afa1780038715f8558489eaaa005f8b
Ram900335 <96827712+Ram900335@users.noreply.github.com>
2021-12-29 19:04:53 +0530

    Add files via upload

---
+++ leapYeaer_without_if_6/Program.cs
@@ -0,0 +1,21 @@
+﻿//6)find a given year is leap year or not without using if block
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leapYeaer_without_if_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            bool newyear = (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) ? true : false;
+            Console.WriteLine(newyear);
+        }
+    }
+}
